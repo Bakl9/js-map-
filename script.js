58537be69c77e0ce1e42884f38c45189d689556f
Makarov ilya <74796903+Bakl9@users.noreply.github.com>
2021-05-13 12:34:31 +0300

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,11 @@ console.log(roots)
 
 var neaArray = array.map((title, index,lol) => title);
 console.log(neaArray)
+
+var x = c.map((title,index, array) => {
+   if(index == 0) {
+    array[index + 1] = 50
+   };
+   return title
+}) 
+console.log(x)
